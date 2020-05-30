749f64c103207ef9730c4f5c1438afa891e32a75
Alex S <alexspringer@pm.me>
2020-05-30 14:38:34 +0100

    package metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netimpact',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'Click',
@@ -13,4 +13,9 @@ setup(
         [console_scripts]
         netimpact=netimpact.netimpact:cli
     ''',
+    description='Cross affiliate network data and partner tools',
+    url='http://github.com/jalexspringer/netimpact',
+    author='Alex Springer',
+    author_email='alexspringer@pm.me',
+    license='MIT',
 )
